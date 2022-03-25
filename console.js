6fa1e84bdf37808a0e3570ba29846207817a0b08
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-03-25 11:33:18 -0700

    console is toggleabel

--- console.js
+++ console.js
@@ -4,6 +4,7 @@ const data = require('./data.json');
 function log(text, __args) {
     //logging
     //get time
+    if (!data.consoleLog) return;
     let date = new Date();
     let time = date.getHours() + ":" + date.getMinutes()
 
@@ -12,6 +13,7 @@ function log(text, __args) {
 
 function debug(text, __args) {
     //debugging
+    if (!data.consoleLog) return;
     if (data.debug){
         let date = new Date();
         let time = date.getHours() + ":" + date.getMinutes()
@@ -21,6 +23,7 @@ function debug(text, __args) {
 
 function warn(text, __args) {
     //warning
+    if (!data.consoleLog) return;
     let date = new Date();
     let time = date.getHours() + ":" + date.getMinutes()
     console.log(`[${time}][Warning]:`, chalk.yellow(text));
@@ -28,6 +31,7 @@ function warn(text, __args) {
 
 function error(text, __args) {
     //error
+    if (!data.consoleLog) return;
     let date = new Date();
     let time = date.getHours() + ":" + date.getMinutes()
     console.log(`[${time}][Error]:`,chalk.red(text));
@@ -35,12 +39,14 @@ function error(text, __args) {
 
 function news(text, __args) {
     //news
+    if (!data.consoleLog) return;
     let date = new Date();
     let time = date.getHours() + ":" + date.getMinutes()
     console.log(`[${time}][News]:`,chalk.magentaBright(text));
 }
 
 function blankSpace(){
+    if (!data.consoleLog) return;
     console.log("")
 }
 
